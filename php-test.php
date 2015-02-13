164ebe253fb098af6fdbd572de76ddf1a2bf7402
h.nakazato <h.nakazato@leihauoli.com>
2015-02-13 11:55:09 +0900

    first-commit header-template

---
+++ php-test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>phpインクルードテスト</title>
+</head>
+<body>
+
+
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/mine/header.html'); ?>
+<!-- ルートパスでの指定 -->
+
+
+</body>
+</html>
